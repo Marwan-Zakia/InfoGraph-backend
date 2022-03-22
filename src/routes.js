5b5b1061ab0e042ad4c226c4ca337bae27441e45
Marwan-Zakia <marwanamir.ma@gmail.com>
2022-03-22 22:18:19 +0300

    added statues codes

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,15 @@ const users = require("./models/index");
 const basicAuth = require("./middleware/basic");
 const bearerAuth = require("./middleware/bearer");
 const permissions = require("./middleware/acl.js");
-const projectsCollection = require("./models/index");
+const {
+	projectsCollection,
+	usersCollection,
+} = require("./models/index");
 
 authRouter.post("/signup", async (req, res, next) => {
 	console.log(users);
 	try {
-		let userRecord = await users.Users.create(req.body);
+		let userRecord = await usersCollection.create(req.body);
 		const output = {
 			user: userRecord,
 			token: userRecord.token,
@@ -33,35 +36,29 @@ authRouter.get(
 	bearerAuth,
 	permissions("delete"),
 	async (req, res, next) => {
-		const userRecords = await users.Users.findAll({});
+		const userRecords = await usersCollection.get();
 		const list = userRecords.map((user) => user.username);
 		res.status(200).json(list);
 	},
 );
 authRouter.post("/project", async (req, res) => {
-	let projects = await projectsCollection.Project.create(req.body);
+	let projects = await projectsCollection.create(req.body);
 	res.status(201).json(projects);
 });
 authRouter.get("/project", async (req, res) => {
-	let somthing = await projectsCollection.Project.findAll({});
+	let somthing = await projectsCollection.get();
 	res.status(200).json(somthing);
 });
 authRouter.put("/project/:id", async (req, res) => {
 	const id = req.params.id;
 	const obj = req.body;
-	const Newproject = await projectsCollection.Project.findOne({
-		where: { id },
-	});
-	Newproject.update(obj);
-	res.status(201).json(Newproject);
+	const Newproject = await projectsCollection.update(id,obj)
+	res.status(204).json(Newproject);
 });
 authRouter.delete("/project/:id", async (req, res) => {
 	const id = req.params.id;
-	const project = await projectsCollection.Project.findOne({
-		where: { id },
-	});
-	project.destroy({ where: { id } });
-	res.status(200).json(project);
+	const project = await projectsCollection.delete(id);
+	res.status(202).json(project);
 });
 
 module.exports = authRouter;
